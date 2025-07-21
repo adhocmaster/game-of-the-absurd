442aa107be5cbeec3162c6336994c413c86bea76
ryaalbr <ryaalbr@nuevaschool.org>
2025-07-20 18:57:14 -0700

    debug

--- absurd_soccer.py
+++ absurd_soccer.py
@@ -514,7 +514,7 @@ def turn_ruleset_to_settings(ruleset: str):
 
 def generate_prompt_2_alt_few_shot(ruleset:str, model_group:str):
     game_state, action_state, comparator_state, score_state = turn_ruleset_to_settings(ruleset)
-    #sample_prompts = [x for x in worst_prompts[ruleset + "_" + model_group] if type(x) is str]
+    sample_prompts = [x for x in worst_prompts[ruleset + "_" + model_group] if type(x) is str]
     print(sample_prompts)
     #sample_prompts = [x for x in sample_prompts if x.startswith("Absurd")]
     sample_answers = [x for x in worst_prompts[ruleset + "_" + model_group + "_absurd"] if x in ["team A", "team B", "both teams"]]
